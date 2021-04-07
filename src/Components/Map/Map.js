9fffb96882c87d27bc09a4bcc897817ea40762fa
Kaytlyn Guerrero <kaytlynguerrero@Kaytlyns-MacBook-Air.local>
2021-04-06 22:18:22 -0500

    KG added map

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,38 @@
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+import React from 'react';
+//import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+
+import { Component } from "react"
+
+//mapboxgl.workerClass = MapboxWorker;
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2F5dGx5bmd1ZXJyZXJvIiwiYSI6ImNrbGd5ejMwMTB5cDQyd29paGZoYnE0dW4ifQ.FN8hXcvlYPaxkl0UdIatVQ';
+
+class Map extends Component {
+    constructor(props) {
+        super(props); 
+        this.state = { 
+          coordinateRoute: props.coordinateRoute,
+          center: props.coordinateRoute[props.coordinateRoute.length-1],
+          zoom: 12
+        };
+        this.mapContainer = React.createRef();
+      }
+      componentDidMount() {
+        const { center, zoom } = this.state;
+        const map = new mapboxgl.Map({
+          container: this.mapContainer.current, // container option  tells mapbox GL JS to render the map inside a specific DOM element.
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: center,
+          zoom: zoom
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+            <div ref={this.mapContainer} className = "map-container" /> 
+          </div>
+        )
+    }
+}
+export default Map
--- src/Demand/OrderPage/OrderPage.js
+++ src/Demand/OrderPage/OrderPage.js
@@ -1,3 +1,4 @@
+import Map from "../../Components/Map/Map"
 import { connect } from "react-redux";
 import AnimatedCar from "../../Components/AnimatedCar/AnimatedCar";
 
@@ -5,14 +6,16 @@ const mapStateToProps = ({ order: { currentOrder } }) => ({
   order: currentOrder,
 });
 
+const coordinateRoute = ([[-97.758917, 30.231775], [-97.758166, 30.232976], [-97.757988,
+  30.232891], [-97.757622, 30.232621], [-97.757501, 30.232374], [-97.757427, 30.23205]])
 const OrderPage = ({ order }) => {
   console.log(order);
   return (
     <>
       <p className="primarySize tc">Order Page</p>
       <p className="secondarySize tc">Confirmation# {order.publicId}</p>
-      <div className="center ">
-        <AnimatedCar />
+      <div className="" >
+      <Map coordinateRoute = {coordinateRoute}/> 
       </div>
     </>
   );
