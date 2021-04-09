0a573a25f62eb27c39c341ba44db060898306a66
Kaytlyn Guerrero <kaytlynguerrero@Kaytlyns-MacBook-Air.local>
2021-04-08 22:08:28 -0500

    KG added route layer to map

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,6 +2,7 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 import React from 'react';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 import { Component } from "react"
 
@@ -26,12 +27,64 @@ class Map extends Component {
           center: center,
           zoom: zoom
         });
+        // var geocoder = new MapboxGeocoder({
+        //   accessToken: mapboxgl.accessToken,
+        //   mapboxgl: mapboxgl
+        //   });
+           
+        //   document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+        map.on('load', function () {
+          map.addSource('route', {
+          'type': 'geojson',
+          'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+          'type': 'LineString',
+          'coordinates': [
+            [-97.743127, 30.267215],
+            [-97.744555, 30.263397],
+            [-97.736771, 30.261224],
+            [-97.73722, 30.260014],
+            [-97.737208, 30.256924],
+            [-97.735073, 30.246365],
+            [-97.735061, 30.244821],
+            [-97.735662, 30.243183],
+            [-97.741481, 30.233463],
+            [-97.747739,30.223529],
+            [-97.748971, 30.224411],
+            [-97.754122, 30.226749],
+            [-97.752933, 30.228932]
+          ]
+            }
+          }
+          });
+          map.addLayer({
+              'id': 'route',
+              'type': 'line',
+              'source': 'route',
+              'layout': {
+              'line-join': 'round',
+              'line-cap': 'round'
+              },
+              'paint': {
+              'line-color': 'black',
+              'line-width': 8
+          }
+          });
+          });
+
+
+
+        
     }
     
     render() {
         return (
             <div>
             <div ref={this.mapContainer} className = "map-container" /> 
+            {/* <div id="geocoder" class="geocoder"></div> */}
           </div>
         )
     }
