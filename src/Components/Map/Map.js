ea1855acd07ee596b4385ababe155e7680e2b94d
Kaytlyn Guerrero <kaytlynguerrero@Kaytlyns-MacBook-Air.local>
2021-04-20 22:28:07 -0500

    KG - added animated vehicle to route

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,9 +2,12 @@ import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
 import React from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import * as turf from '@turf/turf';
 
 import { Component } from "react";
+
+
 
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken =
@@ -31,12 +34,7 @@ class Map extends Component {
       center: center,
       zoom: zoom,
     });
-    // var geocoder = new MapboxGeocoder({
-    //   accessToken: mapboxgl.accessToken,
-    //   mapboxgl: mapboxgl
-    //   });
 
-    //   document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
     map.on("load", function () {
       map.addSource("route", {
@@ -60,18 +58,71 @@ class Map extends Component {
         },
         paint: {
           "line-color": "black",
-          "line-width": 8,
+          "line-width": 5,
         },
       });
+    
+      var data = {"geometry": {
+        "type": "Point", 
+        "coordinates": coordinateRoute[0]},
+        "type": "Feature", 
+        "properties": {}}
+
+    var i = 0;
+    var animate = setInterval(() => {
+      var data = {"geometry": {
+        "type": "Point", 
+        "coordinates": coordinateRoute[i]},
+        "type": "Feature", 
+        "properties": {}}
+      map.getSource("vehicle").setData(data)
+      map.flyTo({
+        center: data.geometry.coordinates,
+        speed: 0.5
+      })
+      i++;
+      if(i >= coordinateRoute.length){
+        clearInterval(animate);
+        alert("Your vehicle has arrived!")
+        // TODO: We will call the vehicle API to set the vehicle to available and request
+        // the next leg of the trip. When we call the vehicle api to set vehicle coordinates
+        // we will set it at the last coordinate to update in the DB
+        
+      }
+
+    }, 2000);
+    console.log("done")
+    map.addSource("vehicle", {
+      'type': 'geojson',
+        data: data
     });
+    map.addLayer({
+      'id': 'vehicle',
+      'type': 'symbol',
+      'source': 'vehicle',
+      'layout': {
+        'icon-image': 'car-15'
+      }
+    })
+
+      
+      });
+
   }
+
+  
 
   render() {
     return (
       <div>
+        {/* <div id="map"></div>
+        <div class="overlay">
+        <button id="replay">Replay</button>
+        </div> */}
         <div ref={this.mapContainer} className="map-container" />
         {/* <div id="geocoder" class="geocoder"></div> */}
       </div>
+      
     );
   }
 }
